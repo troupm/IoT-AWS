e682fd1399072457ac13a43c873d703a85bb6f58
tmiller <tmiller@amherst.com>
2018-11-13 14:36:13 -0600

    uuid

--- led-example.js
+++ led-example.js
@@ -1,11 +1,13 @@
 const awsIot = require('aws-iot-device-sdk');
+const uuid = require('uuid');
 const ThingHost = 'a3knx5ouu01ymf-ats.iot.us-east-1.amazonaws.com';
 const KeyPath = 'AIL_IoT_RPi_01.private.key';
 const CertPath = 'AIL_IoT_RPi_01.cert.pem';
 const CaPath = 'root-CA.crt';
-const ThingName = 'AIL_IoT_RPi_01';
+const ThingName = `AIL_IoT_RPi_01${uuid.v4()}`;
 const Port = 8883;
 const Region = 'us-east-1';
+
 
 var ledToggle = false;
 var connected = false;
